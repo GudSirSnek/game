bf21c92738e120909b5ec292a32333a60aac3adf
Timothy <58702543+GudSirSnek@users.noreply.github.com>
2022-01-22 02:14:40 +0100

    working window

---
+++ engine/include/engine.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "util.h"
+#include "maths.h"
+#include <string>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define FALSE 0
+#define TRUE 1
+
+#define FPS 60
+#define FRAME_TARGET_TIME (1000 / FPS)
+
+
+class Game
+{
+    SDL_Window* window;
+    bool running;
+
+    public:
+        SDL_Renderer* renderer;
+
+
+        Game();
+
+        void init(const char *title, int width, int height);
+
+        SDL_Window* createWindow(const char *title, int width, int height);
+        SDL_Renderer* createRenderer(void);
+
+        void handleEvents();
+        void update();
+
+        void render();
+
+
+        bool isRunning();
+
+        void clean(void);
+
+        void clearScreen(int r, int g, int b, int a);
+        void startRender(void);
+        void endRender(void);
+
+        ~Game();
+
+};
---
+++ engine/src/engine.cpp
@@ -0,0 +1,107 @@
+#include <engine.h>
+#include <stdio.h>
+
+int last_frame_time = 0;
+int imgFlags = IMG_INIT_PNG;
+uint32_t w_flags = 0;
+
+
+Game::Game(){
+    Game::window =nullptr;
+    Game::renderer = nullptr;
+    Game::running = false;
+}
+
+Game::~Game(){
+
+}
+
+void Game::init(const char *title, int width, int height){
+
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        pe_printError("Failed to Initialize SDL: %s", SDL_GetError());
+    }
+    else if ((IMG_Init(imgFlags) & imgFlags) != imgFlags){
+        pe_printFatalError("Failed to Initialize SDL_img: %s", IMG_GetError());
+    }
+
+    pe_printInfo("successfully initialized Photonica Engine", NULL);
+
+    Game::createWindow(title, width, height);
+    Game::createRenderer();
+
+    Game::running = true;
+}
+
+SDL_Window* Game::createWindow(const char *title, int width, int height){
+    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, w_flags);
+    if (!window){
+        pe_printFatalError("ERROR INITIALIZING WINDOW.", SDL_GetError());
+        return NULL;
+    }
+    return window;
+}
+
+SDL_Renderer* Game::createRenderer(void){
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer){
+        pe_printFatalError("ERROR INITIALIZING RENDERER.", SDL_GetError());
+        return NULL;
+    }
+
+    return renderer;
+}
+
+void Game::handleEvents(){
+    SDL_Event event;
+    SDL_PollEvent(&event);
+        switch(event.type){
+            case SDL_QUIT:
+                Game::running = false;
+                break;
+            default:
+                break;
+        }
+}
+void Game::update(void) {
+    // Calculate how much we have to wait until we reach the target frame time
+    int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - last_frame_time);
+
+    // Only delay if we are too fast too update this frame
+    if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME)
+        SDL_Delay(time_to_wait);
+
+    // Get a delta time factor converted to seconds to be used to update my objects
+    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0;
+
+    // Store the milliseconds of the current frame
+    last_frame_time = SDL_GetTicks();
+    //printf("\n");
+}
+
+void Game::render(){
+    SDL_RenderClear(Game::renderer);
+
+    //rendering
+
+    SDL_RenderPresent(Game::renderer);
+}
+
+bool Game::isRunning(){
+    return Game::running;
+}
+
+void Game::clean(void){
+    pe_printNeutral("uniniting renderer, window, SDL", NULL);
+    SDL_DestroyRenderer(Game::renderer);
+    pe_printNeutral("renderer uninitialized",NULL);
+    SDL_DestroyWindow(Game::window);
+    pe_printNeutral("window uninitialized",NULL);
+    IMG_Quit();
+    pe_printNeutral("SDL_IMG uninitialized",NULL);
+    SDL_Quit();
+    pe_printNeutral("SDL uninitialized",NULL);
+
+    pe_printInfo("Photonica Engine successfully uninitialized",NULL);
+}
